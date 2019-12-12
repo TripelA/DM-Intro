3ab13ee721e6d85e20fd241871bfc95f32a23dd8
TripelA <anamay9707@gmail.com>
2019-12-12 12:54:13 -0500

    Add files via upload

---
+++ keys.py
@@ -0,0 +1,5 @@
+keychain = {'twitter':{'api_secret':'n36oXZdz1VNq7PpEIrG6p4XTtM0JvhQSW5GadGrERr3KA3yGWP' ,
+                'api_key':'gA3eMSTXpjl4o79CZWBoXsRyv' ,
+                       'access_token':'1478856445-Mcu59wfS67COEH5iZ3QVYQBgNEuowRkHoNabUX0' ,
+                       'access_token_secret':'xawnupi5xx828VbrrRJChSaSQC5kt1u2P3XJXhZavnkpr'},
+           'MySQL' : {'userid': 'ctim' , 'userpass':'DoLoop@222014'}}
